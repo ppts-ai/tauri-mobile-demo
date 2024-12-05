207bd60684e17fb64edb2c91586b646c7db6dd0b
Jason Hao <jasonhao518@gmail.com>
2024-12-05 21:58:12 +0100

    add tailwind and shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -15,7 +16,7 @@ function App() {
   return (
     <main className="container">
       <h1>Welcome to Tauri + React</h1>
-
+      <Button>Click me</Button>
       <div className="row">
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
